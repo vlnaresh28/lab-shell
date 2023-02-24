d32e8f16394ff081939ded7552dcba0a6657cac4
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 17:49:11 +0000

    mono

--- mongodb.sh
+++ mongodb.sh
@@ -9,9 +9,6 @@ yum install mongodb-org -y  &>>${log_file}
 status_check $?
 
 
-print_head "Update MongoDB Listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
-status_check $?
 
 
 print_head "enable Mongodb "

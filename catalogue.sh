0e7e8a7bfa3915aa4ae1abe6e350fbf867bb0550
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 10:24:30 +0000

    cat12

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,38 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install 
+source common.sh
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+yum install nodejs -y &>>${log_file}
+status_check $?
+useradd roboshop&>>${log_file}
+status_check $?
+mkdir /app &>>${log_file}
+status_check $?
+rm -rf /app/*&>>${log_file}
+status_check $?
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip&>>${log_file}
+status_check $?
+cd /app&>>${log_file}
+status_check $?
+unzip /tmp/catalogue.zip&>>${log_file}
+status_check $?
+cd /app&>>${log_file}
+status_check $?
+npm install &>>${log_file}
+status_check $?
 
 cp configs/catalogue.service  /etc/systemd/system/catalogue.service
+&>>${log_file}
+status_check $?
+systemctl daemon-reload&>>${log_file}
+status_check $?
+systemctl enable catalogue &>>${log_file}
+status_check $?
+systemctl start catalogue&>>${log_file}
+status_check $?
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
-
-cp configs/mongodb.repo  /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.learndevopseasy.online </app/schema/catalogue.js
+cp configs/mongodb.repo  /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+mongo --host mongodb.learndevopseasy.online </app/schema/catalogue.js &>>${log_file}
+status_check $?

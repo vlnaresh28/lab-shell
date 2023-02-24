7a29258600ae3d4ff238bb9dd8a5b8bd95cc0e38
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 10:19:42 +0000

    common_sh

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,4 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head() {
-  echo -e "\e[36m$1\e[0m"
-}
-
-status_check() {
-  if [ $1 -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-    echo "Read the log file ${log_file} for more information about error"
-    exit 1
-  fi
-}
+source common.sh
 
 
 print_head "Installaling Nginx "
--- mongodb.sh
+++ mongodb.sh
@@ -1,22 +1,4 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-
-print_head() {
-  echo -e "\e[36m$1\e[0m"
-}
-
-status_check() {
-  if [ $1 -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-    echo "Read the log file ${log_file} for more information about error"
-    exit 1
-  fi
-}
-
+source common.sh
 
 print_head "Setup Mongodb repository "
 cp ${code_dir}/configs/mongodb.repo  /etc/yum.repos.d/mongo.repo &>>${log_file}

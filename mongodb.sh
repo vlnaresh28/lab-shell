a8381901002f667f2efe432099207c6cac40e03b
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 17:51:24 +0000

    mon22

--- mongodb.sh
+++ mongodb.sh
@@ -8,6 +8,9 @@ print_head "Installaling MongoDB "
 yum install mongodb-org -y  &>>${log_file}
 status_check $?
 
+print_head "Update MongoDB Listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>${log_file}
+status_check $?
 
 
 

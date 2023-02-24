91bbfec83aaaeba0387b0c019a389f44b7f1f0a0
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 09:46:40 +0000

    mongo_vari_function

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,35 @@
-cp configs/mongodb.repo  /etc/yum.repos.d/mongodb
-yum install mongodb-org -y 
-systemctl enable mongod 
-systemctl start mongod 
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+
+
+print_head() {
+  echo -e "\e[36m$1\e[0m"
+}
+
+status_check() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    echo "Read the log file ${log_file} for more information about error"
+    exit 1
+  fi
+}
+
+
+print_head "Copying Repo file to repos.d/mongodb "
+cp configs/mongodb.repo  /etc/yum.repos.d/mongodb &>>${log_file}
+status_check $?
+  
+print_head "Installaling MongoDB "
+yum install mongodb-org -y  &>>${log_file}
+status_check $?
+
+print_head "enable Mongodb "
+systemctl enable mongod &>>${log_file}
+status_check $?
+ 
+print_head " Start Mongodb "
+systemctl start mongod  &>>${log_file}
+status_check $? 

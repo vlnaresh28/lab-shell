a6a76549cc6ad37f585887fa78944d7d58633434
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-24 10:34:04 +0000

    cata_print

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,68 @@
 source common.sh
+
+print_head "Configure Nodejs repo "
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 status_check $?
+
+print_head "Installaling Nodejs "
 yum install nodejs -y &>>${log_file}
 status_check $?
+
+print_head "creating roboshop user "
 useradd roboshop&>>${log_file}
 status_check $?
+
+
 mkdir /app &>>${log_file}
 status_check $?
+
+print_head "Removing old content "
 rm -rf /app/*&>>${log_file}
 status_check $?
+
+print_head "Downloading .zip files"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip&>>${log_file}
 status_check $?
+
 cd /app&>>${log_file}
 status_check $?
+
+print_head "unziping the catalogue.zip "
 unzip /tmp/catalogue.zip&>>${log_file}
 status_check $?
+
+print_head "moving to Application Directory "
 cd /app&>>${log_file}
 status_check $?
+
+print_head "Installaling NPM "
 npm install &>>${log_file}
 status_check $?
 
-cp configs/catalogue.service  /etc/systemd/system/catalogue.service
-&>>${log_file}
+print_head "Copy Catalogue service file "
+cp configs/catalogue.service  /etc/systemd/system/catalogue.service &>>${log_file}
 status_check $?
+
+print_head "Reloading Demon tool "
 systemctl daemon-reload&>>${log_file}
 status_check $?
+
+print_head "Enabling Catalogue service "
 systemctl enable catalogue &>>${log_file}
 status_check $?
+
+print_head " Start Catalogue service "
 systemctl start catalogue&>>${log_file}
 status_check $?
 
+print_head " copy the monogodb.repo file "
 cp configs/mongodb.repo  /etc/yum.repos.d/mongo.repo &>>${log_file}
 status_check $?
+
+print_head "Installaling Mongodb Client "
 yum install mongodb-org-shell -y &>>${log_file}
-status_check $?
+status_check $
+
+print_head "loading Mongodb Schema "
 mongo --host mongodb.learndevopseasy.online </app/schema/catalogue.js &>>${log_file}
 status_check $?

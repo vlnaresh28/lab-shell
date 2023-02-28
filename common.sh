34435081f2724c37f3d2ff1accf91fa89739d0ba
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-28 07:01:02 +0000

    -dev

--- common.sh
+++ common.sh
@@ -16,94 +16,96 @@ status_check() {
   fi
 }
 
-
-
-schema_setup () {
-
-if [ "${schema_type}" == "mongo" ] ; then
-
-  print_head " copy the monogodb.repo file "
-  cp ${code_dir}/configs/mongodb.repo  /etc/yum.repos.d/mongo.repo &>>${log_file}
+systemd_setup() {
+  print_head "Copy SystemD Service File"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
   status_check $?
 
-  print_head "Installaling Mongodb Client "
-  yum install mongodb-org-shell -y &>>${log_file}
+  sed -i -e "s/ROBOSHOP_USER_PASSWORD/${roboshop_app_password}/" /etc/systemd/system/${component}.service &>>${log_file}
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${log_file}
   status_check $?
 
-  print_head "loading Mongodb Schema "
-  mongo --host mongodb.learndevopseasy.online </app/schema/${component}.js &>>${log_file}
+  print_head "Enable ${component} Service "
+  systemctl enable ${component} &>>${log_file}
   status_check $?
 
-fi
-
+  print_head "Start ${component} Service"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
 }
 
+schema_setup() {
+  if [ "${schema_type}" == "mongo" ]; then
+    print_head "Copy MongoDB Repo File"
+    cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+    status_check $?
 
-nodejs (){
-  
-print_head "Configure Nodejs repo "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-status_check $?
+    print_head "Install Mongo Client"
+    yum install mongodb-org-shell -y &>>${log_file}
+    status_check $?
 
-print_head "Installaling Nodejs "
-yum install nodejs -y &>>${log_file}
-status_check $?
+    print_head "Load Schema"
+    mongo --host mongodb-dev.devopsb71.online </app/schema/${component}.js &>>${log_file}
+    status_check $?
+  elif [ "${schema_type}" == "mysql" ]; then
+    print_head "Install MySQL Client"
+    yum install mysql -y &>>${log_file}
+    status_check $?
 
-print_head "Create Roboshop user"
+    print_head "Load Schema"
+    mysql -h mysql-dev.devopsb71.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+    status_check $?
+  fi
+}
+
+app_prereq_setup() {
+  print_head "Create Roboshop User"
   id roboshop  &>>${log_file}
   if [ $? -ne 0 ]; then
     useradd roboshop &>>${log_file}
   fi
-  status_check $? 
-  
-print_head "Create Application Directory"
+  status_check $?
+
+  print_head "Create Application Directory"
   if [ ! -d /app ]; then
     mkdir /app &>>${log_file}
   fi
   status_check $?
 
+  print_head "Delete Old Content"
+  rm -rf /app/* &>>${log_file}
+  status_check $?
 
-print_head "Removing old content "
-rm -rf /app/* &>>${log_file}
-status_check $?
+  print_head "Downloading App Content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  status_check $?
+  cd /app
 
-print_head "Downloading .zip files"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-status_check $?
+  print_head "Extracting App Content"
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+}
 
-cd /app &>>${log_file}
+nodejs() {
+  print_head "Configure NodeJS Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
 
-print_head "unziping the ${component}.zip "
-unzip /tmp/${component}.zip &>>${log_file}
-status_check $?
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
 
-print_head "moving to Application Directory "
-cd /app &>>${log_file}
-status_check $?
+  app_prereq_setup
 
-print_head "Installaling NPM "
-npm install &>>${log_file}
-status_check $?
+  print_head "Installing NodeJS Dependencies"
+  npm install &>>${log_file}
+  status_check $?
 
-print_head "Copy ${component} service file "
-cp ${code_dir}/configs/${component}.service  /etc/systemd/system/${component}.service &>>${log_file}
-status_check $?
+  schema_setup
 
-print_head "Reloading Demon tool "
-systemctl daemon-reload &>>${log_file}
-status_check $?
-
-print_head "Enabling ${component} service "
-systemctl enable ${component} &>>${log_file}
-status_check $?
-
-print_head " Start ${component} service "
-systemctl start ${component} &>>${log_file}
-status_check $?
-
-schema_setup
-
-systemd_setup
+  systemd_setup
 
 }
 
@@ -122,6 +124,22 @@ java() {
 
   # Schema Setup Function
   schema_setup
+
+  # SystemD Function
+  systemd_setup
+}
+
+python() {
+
+  print_head "Install Python"
+  yum install python36 gcc python3-devel -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Download Dependencies"
+  pip3.6 install -r requirements.txt &>>${log_file}
+  status_check $?
 
   # SystemD Function
   systemd_setup

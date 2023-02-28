6d8a4c9128069d6bfa582261fc3463f4917a2784
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-28 07:45:45 +0000

    shipping ipaddress

--- shipping.sh
+++ shipping.sh
@@ -12,5 +12,5 @@ systemctl daemon-reload
 systemctl enable shipping 
 systemctl start shipping
 yum install mysql -y 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+mysql -h mysql-dev.learndevopseasy.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 systemctl restart shipping

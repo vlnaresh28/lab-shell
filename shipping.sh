1318126bacece1669ff73b1109648a0cb8f28b48
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-28 08:08:02 +0000

    ship

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,12 @@
-yum install maven -y
+source common.sh
 
-useradd roboshop
-mkdir /app 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
-cd /app 
-unzip /tmp/shipping.zip
-cd /app 
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar 
-systemctl daemon-reload
-systemctl enable shipping 
-systemctl start shipping
-yum install mysql -y 
-mysql -h mysql-dev.learndevopseasy.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
-systemctl restart shipping
+mysql_root_password=$1
+
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+  exit 1
+fi
+
+component=shipping
+schema_type="mysql"
+java
